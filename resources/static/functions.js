5e361eb6f1cc66d830b4d9438252994ee3765277
Brigita <bpbperic@gmail.com>
2023-11-25 18:18:42 +0100

    usklađivanje dizajna, funkcije za unos u tablicu ne rade

--- resources/static/functions.js
+++ resources/static/functions.js
@@ -21,7 +21,6 @@ function printPatients() {
 }
 
 function submitNewReading() {
-   console.log("submitNewReading function called");
 
     var table = document.getElementById("pregled_stanja_table");
 
@@ -52,6 +51,32 @@ function submitNewReading() {
 
     document.querySelector('form').reset();
 
-    console.log("Function executed successfully");
+}
+
+function createNewPatient() {
+
+    var table = document.getElementById("patients_list");
+
+    var row = document.createElement("tr");
+
+    var td1 = document.createElement("td");
+    var td2 = document.createElement("td");
+    var td3 = document.createElement("td");
+    var td4 = document.createElement("td");
+
+    td1.innerHTML = document.getElementById("ime_novog_korisnika").value;
+    td2.innerHTML = document.getElementById("prezime_novog_korisnika").value;
+    td3.innerHTML = document.getElementById("oib_novog_korisnika").value;
+    td4.innerHTML = document.getElementById("datum_rodenja_novog_korisnika").value;
+
+    row.appendChild(td1);
+    row.appendChild(td2);
+    row.appendChild(td3);
+    row.appendChild(td4);
+
+    table.appendChild(row);
+
+    document.querySelector('form').reset();
+
 }
 
