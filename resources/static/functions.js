1d1cead604f04ef7f7414677f4199b090d4b220d
zoranjuras <cmundir@gmail.com>
2023-11-29 17:00:44 +0100

    Dodana datoteka s lozinkama za testere

--- resources/static/functions.js
+++ resources/static/functions.js
@@ -5,7 +5,7 @@ function login(){
 
     if (username == "Doktor" && password == "123"){
         window.location.href = "PatientHistory.html";
-    } else if (username == "Marko_Mihic" && password == "123"){
+    } else if (username == "Pero Deformero" && password == "123"){
         window.location.href = "PatientReadingsEntry.html";
     } else {
         alert ("Wrong username or password! Please try again.");
